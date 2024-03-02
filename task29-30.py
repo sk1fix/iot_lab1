ca28dc9f075890c4c32b42038e3af3e1d44ea721
sk1fix <agafonovtoha427@gmail.com>
2024-03-02 11:54:58 +0400

    /

--- task29-30.py
+++ task29-30.py
@@ -7,4 +7,5 @@
 # Модулем на языке Python является
     #  ✔ Любой текстовый файл с инструкциями на языке Python с расширением .py
     #  Х Любой текстовый файл с инструкциями на языке Python
-    #  Х Любой текстовый файл с инструкциями на языке Python с расширением .pyc
+    #  https://gitlab.com/basicsofprogrammingforsolvingappliedproblemsintechnicalsystems/practices/-/tree/main?ref_type=heads
+    # Х Любой текстовый файл с инструкциями на языке Python с расширением .pyc
